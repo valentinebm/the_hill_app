8cb9661dbe1b4d9f1faca72b8dafb47ceda0b1af
valentinebm <valentine.barbierm@gmail.com>
2016-07-21 21:22:17 +0200

    hill app with fb login

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+  get 'auth/facebook/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
+end
---
+++ db/migrate/20160721181059_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :email, :string
+    add_column :users, :password, :string
+    add_column :users, :name, :string
+  end
+end
