07ed382d27f105c54fffa393ff3f2c11d01ff1dc
valentinebm <valentine.barbierm@gmail.com>
2016-07-22 17:47:28 +0200

    fb login now saving gender and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ApplicationRecord
   def self.create_with_omniauth(auth)
 
     user = find_or_create_by(uid: auth['uid'], provider: auth['provider'])
-    user.email = "#{auth['uid']}@#{auth['provider']}.com"
+    # user.email = "#{auth['uid']}@#{auth['provider']}.com"
+    user.email = auth['info']['email']
     user.password = auth['uid']
     user.name = auth['info']['name']
+    # user.gender = auth['info']['gender']
+    user.gender = auth.extra.raw_info.gender
     user.save!
 
     if User.exists?(user)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721181059) do
+ActiveRecord::Schema.define(version: 20160722152343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160721181059) do
     t.string   "email"
     t.string   "password"
     t.string   "name"
+    t.string   "gender"
   end
 
 end
